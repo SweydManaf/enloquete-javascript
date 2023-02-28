8c1125aa6b77c15460e5071db03000011d0e1457
Sweyd Manaf <abdulsweyd@gmail.com>
2023-02-28 16:26:53 +0200

    Update tabuleiroDeXadrez.js

--- 2-EstruturaDoPrograma/tabuleiroDeXadrez.js
+++ 2-EstruturaDoPrograma/tabuleiroDeXadrez.js
@@ -2,21 +2,11 @@ var size = 8
 var tabuleiro = ""
 
 for (var rows = 1; rows <= size; rows++){
-    if (rows % 2 == 0) {
-        for (var columns = 1; columns <= size; columns++){
-            if (columns % 2 == 0) {
-                tabuleiro += "#"
-            } else{
-                tabuleiro += " "
-            }
-        }
-    } else{
-        for (var columns = 1; columns <= size; columns++){
-            if (columns % 2 != 0) {
-                tabuleiro += "#"
-            } else{
-                tabuleiro += " "
-            }
+    for (var columns = 1; columns <= size; columns++){
+        if ((rows+columns) % 2 == 0) {
+            tabuleiro += "#"
+        } else{
+            tabuleiro += " "
         }
     }
     tabuleiro += "\n"
