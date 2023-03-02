f8d6436355b5d0caea847e2a8cd21c27ed9db21d
Sweyd Manaf <abdulsweyd@gmail.com>
2023-03-02 21:41:39 +0200

    Update aSomaDeUmIntervalo.js

--- 4-EstruturaDeDados-Objetos&Array/aSomaDeUmIntervalo.js
+++ 4-EstruturaDeDados-Objetos&Array/aSomaDeUmIntervalo.js
@@ -1,17 +1,27 @@
-function range(start, end) {
+function range(start, end, increment = 1) {
   let lista = [];
-  for (let run = start; run <= end; run++) {
-    lista.push(run);
+  if (start < end) {
+    for (let run = start; run <= end; run += increment) {
+      lista.push(run);
+    }
+  } else {
+    if (start > end) {
+      for (let run = start; run >= end; run += increment) {
+        lista.push(run);
+      }
+    }
   }
   return lista;
 }
 
 function sum(array) {
   let soma = 0;
-  for (item in array){
-    soma += array[item]
+  for (item in array) {
+    soma += array[item];
   }
-  return soma
+  return soma;
 }
 
-console.log(sum(range(1, 10)));
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)))
