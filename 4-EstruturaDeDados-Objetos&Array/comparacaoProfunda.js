7b918f57da318385e5f1f1ddd3fddd59c28b681b
Sweyd Manaf <abdulsweyd@gmail.com>
2023-03-05 21:25:38 +0200

    Create comparacaoProfunda.js

---
+++ 4-EstruturaDeDados-Objetos&Array/comparacaoProfunda.js
@@ -0,0 +1,12 @@
+function deepEqual(obj1, obj2) {
+    if (){
+
+    } else{
+        return false
+    }
+}
+
+var obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}))
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}))
