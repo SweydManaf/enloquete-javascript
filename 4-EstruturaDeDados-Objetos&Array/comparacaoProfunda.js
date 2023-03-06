ddaca81ea86b4dbefab2ac5cde13b7fbcb5791ca
Sweyd Manaf <abdulsweyd@gmail.com>
2023-03-06 23:06:35 +0200

    Update comparacaoProfunda.js
    failed

--- 4-EstruturaDeDados-Objetos&Array/comparacaoProfunda.js
+++ 4-EstruturaDeDados-Objetos&Array/comparacaoProfunda.js
@@ -1,12 +1,20 @@
 function deepEqual(obj1, obj2) {
-    if (){
-
-    } else{
-        return false
+    if (!(typeof obj1 == 'object' && obj1 != null) && !(typeof obj2 == 'object' && obj2 != null)) return false;
+    
+    let keysObj1 = Object.keys(obj1);
+    let keysObj2 = Object.keys(obj2);
+    
+    if (keysObj1.length != keysObj2.length) return false;
+    
+    for (let key of keysObj1){
+        if (!keysObj2.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
     }
+
+    return true;
 }
 
 var obj = { here: { is: "an" }, object: 2 };
 console.log(deepEqual(obj, obj));
-console.log(deepEqual(obj, {here: 1, object: 2}))
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}))
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {nome: "Sweyd"}));
